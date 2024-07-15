f220d83315a5429688877fe7bd05f57704203bdf
Harika-yenugu <harikanaidu624@gmail.com>
2024-07-15 20:01:29 +0530

    my first commit

---
+++ GitHub/src/test/java/ProcessofGit/FixTheProject.java
@@ -0,0 +1,15 @@
+package ProcessofGit;
+
+import org.testng.annotations.Test;
+
+public class FixTheProject {
+	
+	
+  @Test
+  public void f() {
+	  
+	  System.out.println("Hello World");
+	  
+	  
+  }
+}
